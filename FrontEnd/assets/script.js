f0e199985ac8afbe52992fa83b7a5d03bde7f402
Debliqui Valentin <debliqui.valentin@outlook.com>
2024-10-31 14:24:45 -0400

    Création de la page et gestion des accès

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,5 +1,15 @@
-import { genererWorks } from "./works.js";
-import { btnAffichage } from "./filter.js";
+import { genererWorks } from "../assets/js/works.js";
+import { btnAffichage } from "../assets/js/filter.js";
 
 btnAffichage();
 genererWorks();
+affichageLoginPage();
+
+// redirect to login page
+function affichageLoginPage(){
+    const lienLoginPage = document.querySelector("#login");
+
+    lienLoginPage.addEventListener( "click", () => {
+        window.location.href = './pages/login.html';
+    })
+}
---
+++ FrontEnd/pages/login.js
@@ -0,0 +1,51 @@
+/**
+ * Function that compares the IDs entered by the user with the Users available in the Api.
+ * And call apiUsers function.
+ */
+async function authenticationUser() {
+  const formLogIn = document.querySelector("#logIn");
+  // Listening to the submit button
+  formLogIn.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // Input recovery
+    const users = {
+      email: event.target.querySelector("[name=email]").value,
+      password: event.target.querySelector("[name=password]").value,
+    };
+    const chargeUtile = JSON.stringify(users);
+    apiUsers();
+  });
+}
+/**
+ * Send a request to the API with serialized users.
+ * And response processing .
+ * 
+ * @param {string} chargeUtile 
+ * @returns {object}
+ */
+async function apiUsers(chargeUtile) {
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: chargeUtile,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erreur dans l’identifiant ou le mot de passe");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      window.sessionStorage.setItem("keys", JSON.stringify(data));
+      // redirects to homePage
+      window.location.href = "../index.html";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      // insetion of "Erreur dans l’identifiant ou le mot de passe" in <p>
+      document.querySelector("p").textContent =
+        "Erreur dans l’identifiant ou le mot de passe";
+    });
+}
+
+authenticationUser();
